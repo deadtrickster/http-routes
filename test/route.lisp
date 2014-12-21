8ff21c06a87e8dd25a483e56ac9a776d11d5f6a5
Ilya Khaprov <dead.trickster@gmail.com>
2014-12-21 08:08:33 -0800

    more tests

--- test/route.lisp
+++ test/route.lisp
@@ -15,6 +15,7 @@
     (http-routes.routes:add-route "/*a/foo/*b" :wild-wildcards)
     (http-routes.routes:add-route "/orders/:order-id" :order-with-id)
     (http-routes.routes:add-route "/orders/:order-id/name" :order-with-id-and-name)
+    (http-routes.routes:add-route "/client/(id):client-id" :client-with-maybe-old-id)
 
     (route-match "/articles/some-article"
                  :simple-static-route nil)
@@ -22,11 +23,15 @@
                  :article-wildcard ((:path . "very-important/thing-happened")))
     (route-match "/zoo/woo/foo/bar/baz"
                  :wild-wildcards ((:a . "zoo/woo")
-                                  (:b . "bar/baz")))    
+                                  (:b . "bar/baz")))
     (route-match "/orders/123"
                  :order-with-id ((:order-id . "123")))
     (route-match "/orders/123/name"
                  :order-with-id-and-name ((:order-id . "123")))
     (route-match "/orders/12/3/name"  ;; multi segment order-id 
-                 nil nil)))
+                 nil nil)    
+    (route-match "/client/123"
+                 :client-with-maybe-old-id ((:order-id . "123")))    
+    (route-match "/client/id123"
+                 :client-with-maybe-old-id ((:order-id . "123")))))
 
