49a4e6ea8e76c6244ff2c6e9f2d7339b4995bbb0
Ilya Khaprov <dead.trickster@gmail.com>
2014-12-21 07:51:30 -0800

    full cycle routes tests

--- test/route.lisp
+++ test/route.lisp
@@ -6,12 +6,19 @@
   `(multiple-value-bind (tag parameters)
        (http-routes.routes:try-match-url ,url)
      (is-true (equalp tag ,tag))
-     (is-true (equalp parameters ,parameters))))
+     (is-true (equalp parameters ,(alexandria:alist-hash-table parameters)))))
 
 (test (full-route-test :compile-at :definition-time)
   (let ((http-routes.routes:*routes* (http-routes.routes:make-routes)))
+    (http-routes.routes:add-route "/articles/*path" :article-wildcard)
     (http-routes.routes:add-route "/articles/some-article" :simple-static-route)
+    (http-routes.routes:add-route "/*a/foo/*b" :wild-wildcards)
 
     (route-match "/articles/some-article"
-                        :SIMPLE-STATIC-ROUTE (make-hash-table))))
+                 :simple-static-route nil)
+    (route-match "/articles/very-important/thing-happened"
+                 :article-wildcard ((:path . "very-important/thing-happened")))
+    (route-match "/zoo/woo/foo/bar/baz"
+                 :wild-wildcards ((:a . "zoo/woo")
+                                  (:b . "bar/baz")))))
 
