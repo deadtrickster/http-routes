aa4edfb65af7959171c44d2ad10485281771f495
Ilya Khaprov <dead.trickster@gmail.com>
2014-12-21 10:50:05 -0800

    use :http-routes as  parent test suite

--- test/package.lisp
+++ test/package.lisp
@@ -10,12 +10,12 @@
 (def-suite :http-routes
   :description "Main test suite for HTTP-ROUTES")
 
-(def-suite :http-routes.matcher
+(def-suite :http-routes.matcher :in :http-routes
   :description "HTTP-ROUTES Matcher tests")
 
-(def-suite :http-routes.parser
+(def-suite :http-routes.parser :in :http-routes
   :description "HTTP-ROUTES Parser tests")
 
-(def-suite :http-routes.route
+(def-suite :http-routes.route :in :http-routes
   :description "HTTP-ROUTES full cycle tests,
 from adding routes to matching and extracting variable values")
